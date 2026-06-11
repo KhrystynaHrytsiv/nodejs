ead677c195040b56040ec5725d6d3756e40acf95
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-06-11 16:50:55 +0300

    hw 1

---
+++ main.js
@@ -0,0 +1,24 @@
+const path = require('node:path');
+const fs = require('node:fs');
+const afs = require('node:fs/promises');
+const readline = require('node:readline/promises');
+
+const reduceEmails = async () =>{
+
+const pathToFile = path.join(process.cwd(), 'emails-2.txt');
+const readStream = fs.createReadStream(pathToFile, 'utf8');
+const rl = readline.createInterface({input:readStream});
+try{
+    for await (const line of rl){
+        const email = line.trim().split(/\s+/).pop();
+        if(email.endsWith('@gmail.com')){
+            await afs.appendFile('gmail.com.txt', email + '\n')
+        }else if(email.endsWith('ukr.net')){
+            await afs.appendFile('ukr.net.txt', email + '\n')
+        }
+    }
+}finally {
+    rl.close()
+}
+}
+ reduceEmails()
