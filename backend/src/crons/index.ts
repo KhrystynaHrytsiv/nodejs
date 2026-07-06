5017898a265cb590bd2bf7effe615e731d5d0d8e
KhrystynaHrytsiv <khrystyna.hrytsiv@gmail.com>
2026-07-06 16:23:21 +0300

    lesson 11 crones (spam)

--- backend/src/crons/index.ts
+++ backend/src/crons/index.ts
@@ -1,10 +1,7 @@
 // import { testCron } from "./testCron";
 // import { removeTokens } from "./removeTokens";
 
-// import { emailCron } from "./emailCron";
-
 export const cronRunner = async () => {
     // testCron.start();
     // removeTokens.start();
-    // emailCron.start();
 };
